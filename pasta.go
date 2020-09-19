c8e9e680bb06072ba9c10b9f8ec2cd3fb1d7941b
krashanoff <lkrashanoff2@gmail.com>
2020-09-18 17:30:17 -0700

    Simple implementation

--- pasta.go
+++ pasta.go
@@ -3,52 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"os/signal"
-	"time"
+	"strconv"
 
 	log "github.com/sirupsen/logrus"
 )
 
 type fsop struct {
-	loc  int64
-	size int
-	msg  []byte
-}
-
-func child(f *os.File, bufSize int64, c chan<- fsop, start int64, end int64) {
-	log.WithFields(log.Fields{
-		"time":        time.Now().UTC(),
-		"serviceArea": fmt.Sprintf("[%d, %d]", start, end),
-	}).Infoln("Spawned child.")
-
-	r := fsop{
-		msg: make([]byte, bufSize),
-	}
-
-	// if no end, then just keep reading.
-	if end == -1 {
-		f.Seek(start, io.SeekStart)
-		r.loc = start
-		for {
-			rc, err := f.Read(r.msg)
-			if err != nil {
-				break
-			}
-			r.size = rc
-			c <- r
-			r.loc += int64(rc)
-		}
-		return
-	}
-
-	rc := bufSize
-	for i := start; i < end; i += rc {
-		r.loc = i
-		r.size, _ = f.ReadAt(r.msg, i)
-		c <- r
-	}
+	f    *os.File // file the op is on
+	loc  int64    // location the file is on
+	size int      // size of the operation
+	msg  []byte   // buffer for the message
 }
 
 func main() {
@@ -59,8 +25,28 @@ func main() {
 	verbose := flag.Bool("v", false, "verbose output")
 	in := flag.String("i", os.Stdin.Name(), "your input `file`, pipe, block device, character device...")
 	out := flag.String("o", os.Stdout.Name(), "your output `file`, pipe, block device, dotmatrix printer...")
-	bs := flag.Int64("bs", 8192, "define the `block size` used for read and write buffers")
+	bs := flag.String("bs", "512B", "define the `block size` used for read and write buffers (e.g. 256, 1M, 1G, ...)")
+	// workers := flag.Uint("w", 2, "number of `worker` processes")
 	flag.Parse()
+
+	// buffer size parsing
+	bufSize, err := strconv.ParseInt((*bs)[:len(*bs)-1], 10, 64)
+	if err != nil {
+		log.Fatalf("Invalid block size: %s", err)
+	}
+	switch (*bs)[len(*bs)-1] {
+	case 'B':
+		bufSize *= 1
+	case 'K':
+		bufSize *= 1000
+	case 'M':
+		bufSize *= 1000000
+	case 'G':
+		bufSize *= 1000000000
+	default:
+		log.Fatalf("Invalid block size unit: %v", (*bs)[len(*bs)-1])
+	}
+	log.Infof("Buffer size is (%d)", bufSize)
 
 	log.SetLevel(log.PanicLevel)
 	if *verbose {
@@ -80,55 +66,77 @@ func main() {
 		log.Fatalf("Failed to open or create output file: %v", err)
 	}
 
-	// try to truncate.
+	// try to truncate the output file.
+	// no big deal if we can't, it just makes things marginally
+	// faster.
 	if err := outFile.Truncate(inFileStat.Size()); err != nil {
 		log.WithFields(log.Fields{
 			"error": err,
 		}).Error("Failed to truncate output file")
 	}
 
-	done := make(chan bool, 1)
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt, os.Kill)
+	done := make(chan bool, 1)       // nominal termination
+	abort := make(chan os.Signal, 3) // early termination
+	signal.Notify(abort, os.Interrupt, os.Kill)
 
 	// the thing that keeps these leaky pipes together.
+	reads := make(chan fsop)
+	writes := make(chan fsop)
+
+	// read from master, fwd writes
 	go func() {
-		reads := make(chan fsop)
-		blocks := inFileStat.Size() / *bs
-		for i := int64(0); i < blocks; i++ {
-			go child(inFile, *bs, reads, *bs*i, *bs*(i+1))
+		for op := range reads {
+			op.msg = make([]byte, bufSize)
+			op.size, _ = op.f.ReadAt(op.msg, op.loc)
+			writes <- op
+			log.WithFields(log.Fields{
+				"location": op.loc,
+				"size":     op.size,
+			}).Info("Read")
 		}
-		go child(inFile, *bs, reads, *bs*blocks, -1)
 
-		recordsOut := blocks
+		close(writes)
+		log.Infoln("Closed writes channel.")
+	}()
 
-		log.Infoln("Listening for fsops")
-		recordsIn := int64(0)
-		for {
-			f := <-reads
-			if recordsIn == recordsOut {
-				break
-			}
-
+	// spawn writing process
+	go func() {
+		for op := range writes {
 			// watch for incomplete writes
 			fields := log.WithFields(log.Fields{
-				"location": f.loc,
-				"size":     f.size,
+				"location": op.loc,
+				"size":     op.size,
 			})
-			if _, err := outFile.WriteAt(f.msg[:f.size], f.loc); err != nil {
+			if _, err := outFile.WriteAt(op.msg[:op.size], op.loc); err != nil {
 				fields.Fatal("Write failure!")
 			} else {
-				fields.Info("Wrote")
+				fields.Info("Write")
 			}
-			recordsIn++
 		}
 
 		done <- true
+		log.Infoln("Sent termination signal.")
+	}()
+
+	// master process
+	go func() {
+		log.Infoln("Spawned master process.")
+
+		for i := int64(0); i < inFileStat.Size(); i += bufSize {
+			reads <- fsop{
+				f:   inFile,
+				loc: i,
+			}
+			log.Infoln("Sent request to worker pool")
+		}
+
+		close(reads)
+		log.Infoln("Closed reads channel.")
 	}()
 
 	// graceful early death
 	go func() {
-		<-stop
+		<-abort
 		log.Infoln("Shutting down...")
 		os.Exit(0)
 	}()
